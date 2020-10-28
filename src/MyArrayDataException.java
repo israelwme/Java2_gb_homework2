7bb458fce2605be9ed6a5ec1b5cb265e618aa468
israelwme <israelwme@gmail.com>
2020-10-28 15:46:14 +0200

    homework2

--- src/MyArrayDataException.java
+++ src/MyArrayDataException.java
@@ -2,7 +2,5 @@ public class MyArrayDataException extends Exception {
 
     public MyArrayDataException(String msg) {
         super(msg);
-
-
     }
 }
--- src/MyArraySizeException.java
+++ src/MyArraySizeException.java
@@ -2,7 +2,5 @@ public class MyArraySizeException extends Exception{
 
     public  MyArraySizeException (String message) {
         super (message);
-
-
     }
 }
